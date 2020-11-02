d84169d145d90fc167bd745065e06945f400af37
Jaskaran Singh <jaskaran1523@gmail.com>
2020-11-01 16:13:42 -0800

    Import components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import './App.css';
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
+import Experience from './components/Experience/Experience';
+import Projects from './components/Projects/Projects';
+import Publications from './components/Publications/Publications';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Navbar />
+			<Home />
+			<About />
+			<Skills />
+			<Experience />
+			<Projects />
+			<Publications />
+			<Contact />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
