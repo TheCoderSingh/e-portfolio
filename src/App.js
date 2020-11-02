648bfe12c8b6a41a3f4543763f2a4caf44f573d8
Jaskaran Singh <jaskaran1523@gmail.com>
2020-11-02 03:33:12 -0800

    Delete About

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-import About from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Experience from './components/Experience/Experience';
 import Projects from './components/Projects/Projects';
@@ -14,7 +13,6 @@ function App() {
 		<div className="App">
 			<Header />
 			<Home />
-			<About />
 			<Skills />
 			<Experience />
 			<Projects />
