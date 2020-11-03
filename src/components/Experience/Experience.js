2065f2a01c666ea45d81cc088bbc19a066bbad58
Jaskaran Singh <jaskaran1523@gmail.com>
2020-11-02 22:59:07 -0800

    Create experience section, Add timeline

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -9,7 +9,153 @@ const Experience = () => {
 				<hr />
 				{/* <p>"Wisdom is the reward of experience and should be shared"</p> */}
 			</div>
-			<div className="content">Experience Content</div>
+			<div className="content">
+				<section id="cd-timeline" className="cd-container">
+					<div className="cd-timeline-block">
+						<div className="cd-timeline-img cd-picture"></div>
+
+						<div className="cd-timeline-content">
+							<h2>Freelancer</h2>
+							<div className="timeline-content-info">
+								<span className="timeline-content-info-title">
+									<i
+										className="fa fa-certificate"
+										aria-hidden="true"
+									></i>
+									Netscrew Technologies
+								</span>
+								<span className="timeline-content-info-date">
+									<i
+										className="fa fa-calendar-o"
+										aria-hidden="true"
+									></i>
+									November 2018 - March 2019
+								</span>
+							</div>
+							<p>
+								Working alongside the designer team implementing
+								the designs, also developing custom solutions to
+								address team necessities.
+							</p>
+							<ul className="content-skills">
+								<li>HTML5</li>
+								<li>CSS3</li>
+								<li>JavaScript</li>
+								<li>jQuery</li>
+								<li>Wordpress</li>
+							</ul>
+						</div>
+					</div>
+
+					<div className="cd-timeline-block">
+						<div className="cd-timeline-img cd-picture"></div>
+
+						<div className="cd-timeline-content">
+							<h2>Software Engineer</h2>
+							<div className="timeline-content-info">
+								<span className="timeline-content-info-title">
+									<i
+										className="fa fa-certificate"
+										aria-hidden="true"
+									></i>
+									Geekers Technologies
+								</span>
+								<span className="timeline-content-info-date">
+									<i
+										className="fa fa-calendar-o"
+										aria-hidden="true"
+									></i>
+									July 2018 - October 2018
+								</span>
+							</div>
+							<p>
+								Working alongside the designer team implementing
+								the designs, also developing custom solutions to
+								address team necessities.
+							</p>
+							<ul className="content-skills">
+								<li>HTML5</li>
+								<li>CSS3</li>
+								<li>JavaScript</li>
+								<li>jQuery</li>
+								<li>Wordpress</li>
+							</ul>
+						</div>
+					</div>
+
+					<div className="cd-timeline-block">
+						<div className="cd-timeline-img cd-picture"></div>
+
+						<div className="cd-timeline-content">
+							<h2>Bootcamp Intern</h2>
+							<div className="timeline-content-info">
+								<span className="timeline-content-info-title">
+									<i
+										className="fa fa-certificate"
+										aria-hidden="true"
+									></i>
+									Statusbrew
+								</span>
+								<span className="timeline-content-info-date">
+									<i
+										className="fa fa-calendar-o"
+										aria-hidden="true"
+									></i>
+									June 2018
+								</span>
+							</div>
+							<p>
+								Working alongside the designer team implementing
+								the designs, also developing custom solutions to
+								address team necessities.
+							</p>
+							<ul className="content-skills">
+								<li>HTML5</li>
+								<li>CSS3</li>
+								<li>JavaScript</li>
+								<li>jQuery</li>
+								<li>Wordpress</li>
+							</ul>
+						</div>
+					</div>
+
+					<div className="cd-timeline-block">
+						<div className="cd-timeline-img cd-picture"></div>
+
+						<div className="cd-timeline-content">
+							<h2>Bootcamp Intern</h2>
+							<div className="timeline-content-info">
+								<span className="timeline-content-info-title">
+									<i
+										className="fa fa-certificate"
+										aria-hidden="true"
+									></i>
+									Statusbrew
+								</span>
+								<span className="timeline-content-info-date">
+									<i
+										className="fa fa-calendar-o"
+										aria-hidden="true"
+									></i>
+									June 2017
+								</span>
+							</div>
+							<p>
+								Working alongside the designer team implementing
+								the designs, also developing custom solutions to
+								address team necessities.
+							</p>
+							<ul className="content-skills">
+								<li>HTML5</li>
+								<li>CSS3</li>
+								<li>JavaScript</li>
+								<li>jQuery</li>
+								<li>Wordpress</li>
+							</ul>
+						</div>
+					</div>
+				</section>
+			</div>
 		</section>
 	);
 };
