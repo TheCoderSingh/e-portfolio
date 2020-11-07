5184940c966cef75a9bed24f1df34351b2c9c596
Jaskaran Singh <jaskaran1523@gmail.com>
2020-11-06 21:08:29 -0800

    Create footer, style footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Footer.scss';
 
 const Footer = () => {
 	return (
 		<footer>
-			<p>Footer Text</p>
+			<p>
+				Designed and Developed By{' '}
+				<a href="https://thecodersingh.com/">Jaskaran Singh</a>
+			</p>
 		</footer>
 	);
 };
